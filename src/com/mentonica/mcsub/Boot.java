d51a2fe5233fd18de9ac25998823374c0d54dcf4
Paradox <paradox@parabot.org>
2013-11-08 14:42:26 +0100

    Added player insert function

--- src/com/mentonica/mcsub/Boot.java
+++ src/com/mentonica/mcsub/Boot.java
@@ -1,11 +1,20 @@
 package com.mentonica.mcsub;
 
 import com.mentonica.mcsub.get.data.Variables;
+import net.milkbowl.vault.permission.Permission;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Boot extends JavaPlugin {
+public class Boot extends JavaPlugin implements Listener {
+    Permission permission;
     public void onEnable() {
+        PluginManager pm = Bukkit.getServer().getPluginManager();
+        pm.registerEvents(this, this);
         getLogger().info("MCSub started!");
         Variables.setServerKey(Bukkit.getServerId());
         Variables.setError(false);
@@ -16,5 +25,14 @@ public class Boot extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("MCSub stopped!");
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        //System.out.println("this seems to work");
+        Player player = event.getPlayer();
+        String rank = permission.getPrimaryGroup(player);
+        String playerName = player.toString();
+        System.out.println(rank);
     }
 }
